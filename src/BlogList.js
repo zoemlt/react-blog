90eae066a9723cec23cd66f5b61254a952580fa1
zoe <53597160+zoeniu@users.noreply.github.com>
2021-12-02 15:57:39 +1300

    Blog list component

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = (props) => {
+    const {blogs, title} = props;
+
+    return (
+        <div className="blogList">
+            <h2>{title}</h2>
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default BlogList
