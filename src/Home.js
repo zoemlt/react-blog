25c225c335c06fa4cfdb75b786326a8b001cf79b
zoe <53597160+zoeniu@users.noreply.github.com>
2021-12-03 11:10:11 +1300

    Loading message

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,21 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([]);
-
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
             .then(res => res.json())
             .then(data => setBlogs(data))
+            .then(setIsPending(false));
     }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
+            { isPending
+            ? <div>Loading...</div>
+            : <BlogList blogs={blogs} title="All Blogs" />
+                }
         </div>
     );
 }
