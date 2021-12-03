7649aaee83ccb57d01fae15ee56c005b87c37cff
zoe <53597160+zoeniu@users.noreply.github.com>
2021-12-04 11:36:54 +1300

    Create form and update values dynamically

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>Zoe's Blog</h1>
+            <h1><Link to="/">Zoe's Blog</Link></h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create">New Blog</Link>
