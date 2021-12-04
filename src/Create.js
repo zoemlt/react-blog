a70e5bd9159573ecc63d275d1e0f8fb7d5b05fdb
zoe <53597160+zoeniu@users.noreply.github.com>
2021-12-04 13:13:32 +1300

    POST request

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,31 @@
 import { useState } from "react";
 
 const Create = () => {
-    const [ title, setTitle] = useState("");
-    const [ body, setBody] = useState("");
-    const [ author, setAuthor] = useState("mario");
+    const [ title, setTitle ] = useState("");
+    const [ body, setBody ] = useState("");
+    const [ author, setAuthor ] = useState("mario");
+    const [ isPending, setIsPending ] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch("http://localhost:8000/blogs", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("New blog added");
+            setIsPending(false);
+        })
+    }
 
     return (
         <div className="create">
             <h2>Add a new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title</label>
                 <input
                     type="text"
@@ -30,7 +47,10 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                { !isPending
+                ? <button>Add Blog</button>
+                : <button disabled>Adding blog...</button>
+                }
             </form>
         </div>
     )
