d4d4a3d80b45e8f5b43ab2f6e37ed79733bfd988
zoe <53597160+zoeniu@users.noreply.github.com>
2021-12-04 13:41:35 +1300

    Delete requests

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`);
-    
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: "DELETE"
+        }).then(() => {
+            history.push("/");
+        })
+    }
+
     return (
         <div className="blog-details">
             { isPending && <div>Loading...</div> }
@@ -14,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}>Delete blog</button>
                 </article>
             )}
         </div>
