564b9a2a0541f9420d07783b542b8d26ebef6d31
zoe <zoejniu@gmail.com>
2022-01-29 16:08:04 +1300

    Update app switch routes to v6 element routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Create from "./components/Create";
 import BlogDetails from "./components/BlogDetails";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound";
 
 function App() {
@@ -11,23 +11,12 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-
-            <Route exact path="/create">
-              <Create />
-            </Route>
-
-            <Route exact path="/blogs/:id">
-              <BlogDetails />
-            </Route>
-
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </Router>
